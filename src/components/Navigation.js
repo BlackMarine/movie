366a4e3273442ded19553590347bcf635a257777
BlackMarine <wkdeo0000@gmail.com>
2021-08-11 15:44:14 +0900

    a 엘리먼트 Link 컴포넌트 바꾸기, 버튼css 추가

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import {Link} from 'react-router-dom';// 페이지 새로고침 안하고 바꾸는 기능
+import './Navigation.css'
 
 function Navigation() {
     return (
-        <div>
-            <a href="/">Home</a>
-            <a href="/about">About</a>
+        <div className="nav">
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
         </div>
     );
 }
