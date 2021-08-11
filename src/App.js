bf6b4c32f8f7bfb683113b198a331aabaac5ca89
BlackMarine <wkdeo0000@gmail.com>
2021-08-11 10:02:54 +0900

    라우터 이해
    /home/introduction으로 접속하면 /, /home, /home/introduction순서로 path props가 있는지 찾아서 들어감
    but, path props에는 /home, /home/introduction 둘다 있기 때문에 /home/introduction으로 접속 시 Home, Introduction이 둘 다 그려지는 것.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
-import Home from './routes/Home';
+// import Home from './routes/Home';
 import './App.css';
+import { HashRouter, Route } from 'react-router-dom';
+import About from './routes/About';
+import Home from './routes/Home';
 
 function App() {
-  return <Home />
+  return (
+    <HashRouter>
+      <Route path="/home">
+        <h1>Home</h1>
+      </Route>
+      <Route path="/home/introduction">
+        <h1>Introduction</h1>
+      </Route>
+      <Route path="/about">
+        <h1>About</h1>
+      </Route>
+    </HashRouter>
+  );
 }
 
 export default App;
