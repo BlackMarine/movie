b541e68b7debb7c07da7f6e11c3d7b7dab38a5ad
BlackMarine <wkdeo0000@gmail.com>
2021-08-11 15:19:49 +0900

    a태그 네비게이션 추가 -> react-router-dom Link로 바꾸기 전

--- src/App.js
+++ src/App.js
@@ -4,19 +4,14 @@ import './App.css';
 import { HashRouter, Route } from 'react-router-dom';
 import About from './routes/About';
 import Home from './routes/Home';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
     <HashRouter>
-      <Route path="/home">
-        <h1>Home</h1>
-      </Route>
-      <Route path="/home/introduction">
-        <h1>Introduction</h1>
-      </Route>
-      <Route path="/about">
-        <h1>About</h1>
-      </Route>
+      <Navigation />
+      <Route path="/" exact={true} component={Home}></Route>
+      <Route path="/about" component={About}></Route>
     </HashRouter>
   );
 }
